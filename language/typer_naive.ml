a9f93a2142370ce69f505d997fefd03e6259e297
martin <martin.zivojinovic@etu.u-bordeaux.fr>
2025-04-03 03:11:47 +0200

    Weak typing

--- language/typer_naive.ml
+++ language/typer_naive.ml
@@ -4,18 +4,52 @@ open Typer_util
 let rec type_expr (counter : Counter.t) (env : type_lang Util.Environment.t)
     (expr : expr) =
   match expr with
-  | App (e, _, _) -> type_expr counter env e
-  | Cst_i _ -> TInt
-  | Cst_b _ -> TBool
-  | Cst_str _ -> TString
-  | Cst_func (b, _) -> type_of_built_in b
-  | Nil _ -> TList ([], TUniv 0)
-  | Unit _ -> TUnit
-  | Var (name, _) -> (
+  | Cst_i (_, a) -> let _ = Annotation.set_type a TInt in (TInt, [])
+  | Cst_b (_, a) -> let _ = Annotation.set_type a TBool in (TBool, [])
+  | Cst_str (_, a) -> let _ = Annotation.set_type a TString in (TString, [])
+  | Cst_func (b, a) -> let t = type_of_built_in b in
+                       let _ = Annotation.set_type a t in (t, [])
+  | Nil (a) -> let t = TList ([], TUniv (Counter.get_fresh counter)) in
+               let _ = Annotation.set_type a t in (t, [])
+  | Unit (a) -> let _ = Annotation.set_type a TUnit in (TUnit, [])
+  | Var (name, a) -> (
       match Util.Environment.get env name with
-      | Some x -> x
+      | Some x -> let _ = Annotation.set_type a x in (x, [])
       | _ -> failwith ("Invalid var name: " ^ name))
-  | IfThenElse (cond, ifb, elseb, _) -> type_expr counter env ifb
-  | Let _ -> failwith "err"
-  | Fun (name, e, _) -> failwith "err"
-  | Ignore (e1, e2, _) -> failwith "err"
+  | IfThenElse (cond, ifb, elseb, a) ->
+     (match (type_expr counter env cond, type_expr counter env ifb, type_expr counter env elseb) with
+                | ((TBool, c0), (t1, c1), (t2, c2)) when t1 = t2 -> let _ = Annotation.set_type a t1 in (t1, c0 @ c1 @ c2)
+                | ((invalid_type, c0), (t1, c1), (t2, c2)) when t1 = t2 -> let _ = Annotation.set_type a t1 in (t1, (TBool, invalid_type)::(c0 @ c1 @ c2))
+                | ((TBool, c0), (t1, c1), (t2, c2))  -> let _ = Annotation.set_type a t1  in (t1, (t1, t2)::(c0 @ c1 @ c2))
+                | ((invalid_type, c0), (t1, c1), (t2, c2)) -> let _ = Annotation.set_type a t1 in (t1, (t1, t2)::(TBool, invalid_type)::(c0 @ c1 @ c2)))
+  | Let (is_rec, name, e1, e2, a) ->
+     let constraints_var_building =
+       if is_rec then
+         let fresh = TUniv (Counter.get_fresh counter) in
+         Util.Environment.add env name fresh;
+         let (t, constraints) = type_expr counter env e1 in
+         (fresh, t)::constraints
+       else
+         let (t, constraints) = type_expr counter env e1 in
+         let _ = Util.Environment.add env name t in
+         constraints in
+     let (t, constraints) = type_expr counter env e2 in
+     let _  = Annotation.set_type a t in
+     let _ = Util.Environment.remove env name in (t, constraints_var_building @ constraints)
+  | Fun (name, e, a) ->
+     let param_t = TUniv (Counter.get_fresh counter) in
+     let _ = Util.Environment.add env name param_t in
+     let (return_t, constraints) = type_expr counter env e in
+     let t = TFunc ([],  param_t, return_t) in
+     let _  = Annotation.set_type a t in
+     let _ = Util.Environment.remove env name in (t, constraints)
+  | Ignore (e1, e2, a) ->
+     let _ = type_expr counter env e1 in
+     let (t, constraints) = type_expr counter env e2 in
+     let _  = Annotation.set_type a t in (t, constraints)
+  | App (f, arg, a) ->
+     let (tf, c1) = type_expr counter env f in
+     let (targ, c2) = type_expr counter env arg in
+     let tret = TUniv (Counter.get_fresh counter) in
+     Annotation.set_type a tret;
+     (tret, (tf, TFunc ([], targ, tret)) :: c1 @ c2)
--- language/typer_util.ml
+++ language/typer_util.ml
@@ -49,13 +49,44 @@ let type_of_built_in (built_in : built_in) =
 
 let rec solve_constraints (constraints : (type_lang * type_lang) list) :
     (int * type_lang) list =
+  let rec substitute_constraint n1 t1 constraints =
+    match constraints with 
+    | [] -> []
+    | (TFunc (l, targ, TUniv (n2)), t2) :: r when n1 = n2 ->
+       substitute_constraint n1 t1 ((TFunc (l, targ, t1), t2) :: r)
+    | (TFunc (l, TUniv (n2), tret), t2) :: r when n1 = n2 ->
+       substitute_constraint n1 t1 ((TFunc (l, t1, tret), t2) :: r)
+
+    | (t2, TFunc (l, targ, TUniv (n2))) :: r when n1 = n2 ->
+       substitute_constraint n1 t1 ((t2, TFunc (l, targ, t1)) :: r)
+    | (t2, TFunc (l, TUniv (n2), tret)) :: r when n1 = n2 ->
+       substitute_constraint n1 t1 ((t2, TFunc (l, t1, tret)) :: r)
+
+    | (TList (l, TUniv (n2)), t2) :: r when n1 = n2 ->
+       substitute_constraint n1 t1 ((TList (l, t1), t2) :: r)
+    | (t2, TList (l, TUniv (n2))) :: r when n1 = n2 ->
+       substitute_constraint n1 t1 ((t2, TList (l, t1)) :: r)
+    
+    | (TUniv n2, t2) :: r  when n1 = n2 -> (t1, t2) :: substitute_constraint n1 t1 r
+    | (t2, TUniv n2) :: r  when n1 = n2 -> (t2, t1) :: substitute_constraint n1 t1 r
+    | c :: r -> c :: substitute_constraint n1 t1 r
+  in
   match constraints with
-  (* à modifier après ça*)
-  | [] -> failwith "solve_constraints not implemented"
-  | ((TFunc _ as f), TBool) :: _ -> raise (Constraint_error (f, TBool))
-  | _ :: t ->
-      ignore substitute_constraint;
-      solve_constraints t
+  | [] -> []
+  | (t1, t2) :: r when t1 = t2 -> solve_constraints r
+
+  | (TUniv x1, TUniv x2) :: r when x1 <= x2 ->
+   (x1, TUniv x2) :: solve_constraints (substitute_constraint x1 (TUniv x2) r)
+  | (TUniv x2, TUniv x1) :: r ->
+     (x1, TUniv x2) :: solve_constraints (substitute_constraint x1 (TUniv x2) r)
+
+  | (TList (_, t1), TList (_, t2)) :: r -> solve_constraints ((t1, t2) :: r)
+  | (TFunc (_, p1, r1), TFunc (_, p2, r2)) :: r -> solve_constraints ((p1, p2) :: (r1, r2) :: r)
+
+  | (TUniv x, t) :: r | (t, TUniv x) :: r
+    ->  (x, t) :: solve_constraints (substitute_constraint x t r)
+  | (t1, t2) :: _ ->
+     raise (Constraint_error (t1, t2))
 
 let instantiate (counter : Counter.t) (type_lang : type_lang) : type_lang =
   (* à modifier*)
